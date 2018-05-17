cba4342fbd5194c59b3a2a988274df6258556d31
Brennan Vincello <brennanv03@gmail.com>
2018-05-17 15:40:37 -0700

    clean up formatting

--- assemblyai/client.py
+++ assemblyai/client.py
@@ -20,7 +20,8 @@ class Client(object):
         self.api = ASSEMBLYAI_URL
 
     def __repr__(self):
-        return 'Client(token=%s)' % self.token[0:8] + '...'
+        concat_token = self.token[0:8] + '..'
+        return 'Client(token=%s)' % concat_token
 
     def train(self, phrases, closed_domain=None, name=None):
         """Create a custom language model."""
